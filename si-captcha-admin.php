43aad1d898bdcaa7e8a0c0b6d6a65024af3a2483
MikeChallis <MikeChallis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-18 20:23:02 +0000

    admin
    git-svn-id: https://plugins.svn.wordpress.org/si-captcha-for-wordpress/trunk@324181 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- si-captcha-admin.php
+++ si-captcha-admin.php
@@ -178,12 +178,10 @@ _e('If you find this plugin useful to you, please consider making a small donati
 
   <div style="width:385px;height:200px; float:left;background-color:white;padding: 10px 10px 10px 20px; border: 1px solid #ddd;">
 		<div>
-			<h3>ThemeFuse Original WP Themes</h3>
-            Try <a href="https://www.e-junkie.com/ecom/gb.php?cl=136641&c=ib&aff=148937" target="_blank">ThemeFuse</a>,
-            they make some amazing original WP themes that have a cool 1 click auto install feature and excellent after care support services.
-            Check out some of their themes!
+			<h3><?php _e('ThemeFuse Original WP Themes', 'si-captcha'); ?></h3>
+            <?php echo sprintf(__('Try <a href="%s" target="_blank">ThemeFuse</a>, they make some amazing original WP themes that have a cool 1 click auto install feature and excellent after care support services. Check out some of their themes!', 'si-captcha'), 'https://www.e-junkie.com/ecom/gb.php?cl=136641&c=ib&aff=148937'); ?>
 		</div>
-        <a href="https://www.e-junkie.com/ecom/gb.php?cl=136641&c=ib&aff=148937" target="_blank"><img title="ThemeFuse" alt="ThemeFuse" src="http://themefuse.com/wp-content/themes/themefuse/images/campaigns/themefuse.jpg" width="375" height="85" /></a>
+        <a href="https://www.e-junkie.com/ecom/gb.php?cl=136641&c=ib&aff=148937" target="_blank"><img title="<?php _e('ThemeFuse', 'si-captcha'); ?>" alt="<?php _e('ThemeFuse', 'si-captcha'); ?>" src="http://themefuse.com/wp-content/themes/themefuse/images/campaigns/themefuse.jpg" width="375" height="85" /></a>
   </div>
   </td>
  </tr>
@@ -533,9 +531,9 @@ foreach ($captcha_difficulty_array as $k => $v) {
 <?php
   if ($si_captcha_opt['si_captcha_donated'] != 'true') { ?>
    </td><td width="350">
-   "I recommend <a href="http://secure.hostgator.com/~affiliat/cgi-bin/affiliates/clickthru.cgi?id=mchallis-sicaptchawp" target="_blank">HostGator Web Hosting</a>. All my sites are hosted there. The prices are great and they offer the most features." - Mike Challis
+   <?php echo sprintf(__('"I recommend <a href="%s" target="_blank">HostGator Web Hosting</a>. All my sites are hosted there. The prices are great and they offer the most features." - Mike Challis', 'si-captcha'), 'http://secure.hostgator.com/~affiliat/cgi-bin/affiliates/clickthru.cgi?id=mchallis-sicaptchawp'); ?>
    </td><td width="100">
-     <a href="http://secure.hostgator.com/~affiliat/cgi-bin/affiliates/clickthru.cgi?id=mchallis-sicaptchawp" target="_blank"><img title="Web Site Hosting" alt="Web Site Hosting" src="<?php echo WP_PLUGIN_URL; ?>/si-captcha-for-wordpress/hostgator-blog.gif" width="100" height="100" /></a>
+     <a href="http://secure.hostgator.com/~affiliat/cgi-bin/affiliates/clickthru.cgi?id=mchallis-sicaptchawp" target="_blank"><img title="<?php echo esc_attr(__('Web Site Hosting', 'si-captcha')); ?>" alt="<?php echo esc_attr(__('Web Site Hosting', 'si-captcha')); ?>" src="<?php echo WP_PLUGIN_URL; ?>/si-captcha-for-wordpress/hostgator-blog.gif" width="100" height="100" /></a>
 <?php
   }
  ?>
